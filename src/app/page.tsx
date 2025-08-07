5f58dd21a33f73d63d460228038238e2182c5c26
Jacob Hummel <j4cobhummel@gmail.com>
2025-08-07 13:52:24 -0500

    make searchTerm controlled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
         </p>
         <input
           style={{ border: "1px solid black" }}
+          value={searchTerm}
           onChange={onSearchTermChange}
         />
         <button onClick={onClick}>Reset Search</button>
